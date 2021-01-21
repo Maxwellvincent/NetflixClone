b748f0bfd57964ebfac2bf289771e8c74886f05e
Louis M <louismaxwell@knights.ucf.edu>
2021-01-21 17:30:35 -0500

    updated project files for jumbotron

--- netflixclone/src/components/jumbotron/index.js
+++ netflixclone/src/components/jumbotron/index.js
@@ -9,15 +9,21 @@ export default function Jumbotron({
   // eslint-disable-next-line no-unused-vars
   ...restProps
 }) {
-  return (
-    <Inner direction={direction}>
-      <p>Hello</p>
-    </Inner>
-  );
+  return <Inner direction={direction}>{children}</Inner>;
 }
 
 // eslint-disable-next-line react/prop-types
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronContainer({ children, ...restProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Pane = function JumbotronContainer({ children, ...restProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Pane {...restProps}>{children}</Pane>;
 };
