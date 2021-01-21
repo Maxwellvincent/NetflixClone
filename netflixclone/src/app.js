94e6c94d26c4e7f0f3bf063dc1cff80e34225026
Louis M <louismaxwell@knights.ucf.edu>
2021-01-20 22:07:59 -0500

    working on jumbotron component

--- netflixclone/src/app.js
+++ netflixclone/src/app.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import jumboData from './fixtures/jumbo.json';
 import Jumbotron from './components/jumbotron';
 
 export default function App() {
-  return <Jumbotron />;
+  return (
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <p>HEllo</p>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
 }
--- netflixclone/src/components/jumbotron/index.js
+++ netflixclone/src/components/jumbotron/index.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React from 'react';
+import { Container, Inner } from './styles/jumbotron';
 
-export default function Jumbotron(){
-    return(<p>I am the Jumbotron</p>);
+export default function Jumbotron({
+  // eslint-disable-next-line react/prop-types
+  children,
+  // eslint-disable-next-line react/prop-types
+  direction = 'row',
+  // eslint-disable-next-line no-unused-vars
+  ...restProps
+}) {
+  return (
+    <Inner direction={direction}>
+      <p>Hello</p>
+    </Inner>
+  );
 }
+
+// eslint-disable-next-line react/prop-types
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Container {...restProps}>{children}</Container>;
+};
