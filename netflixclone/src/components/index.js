9285a7390126648c75284df0da4f9e6acdf2c08e
Louis Maxwell <louismaxwell@knights.ucf.edu>
2021-01-24 15:23:31 -0500

    finished footer styled component

---
+++ netflixclone/src/components/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/prefer-default-export */
+export { default as Jumbotron } from './jumbotron';
+export { default as Footer} from './footer';
---
+++ netflixclone/src/containers/jumbotron.js
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import jumboData from '../fixtures/jumbo';
+import { Jumbotron } from '../components';
+
+export function JumbotronContainer() {
+  return (
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
+}
